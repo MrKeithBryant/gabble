a4a0e965dc6b6aaab6270f7d9f923b5272c33ba9
Keith Bryant <kabryant@cinci.rr.com>
2017-07-11 12:19:41 -0400

    Added a bunch more stuff.

---
+++ migrations/20170710182720-create-gabble.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = {
+  up: function(queryInterface, Sequelize) {
+    return queryInterface.createTable('gabbles', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      music: {
+        type: Sequelize.OBJECT
+      },
+      info: {
+        type: Sequelize.TEXT
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: function(queryInterface, Sequelize) {
+    return queryInterface.dropTable('gabbles');
+  }
+};
